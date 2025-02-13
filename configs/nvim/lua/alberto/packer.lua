1f73a833febf34b7e14aec36db5dfdba20fb463c
albertomolinfelipe <albmf@protonmail.com>
2025-02-13 16:38:59 +0100

    Add fugitive

--- configs/nvim/lua/alberto/packer.lua
+++ configs/nvim/lua/alberto/packer.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
     -- LSP
     use('williamboman/mason.nvim')
